b36389a1fab71718c80e81b8d633617148dc557b
This-is-Luke <lluminous.210@gmail.com>
2024-06-02 15:46:30 +0200

    chore: Update MongoDB connection configuration

--- app/server/config/db.js
+++ app/server/config/db.js
@@ -8,8 +8,6 @@ const connectDB = async () => {
         await mongoose.connect(db, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
         });
 
         console.log('MongoDB Connected...');
